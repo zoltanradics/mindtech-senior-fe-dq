94ba6cd45b552f1313efdb8a0dc2abc103470ea6
Zoltan Radics <zoltan.radics@adnovum.hu>
2025-09-17 18:07:47 +0200

    Introduce table config

---
+++ src/Features/UserList/config.ts
@@ -0,0 +1,25 @@
+
+export const userListTableConfig = [
+  {
+    accessorKey: 'name',
+    header: 'Name',
+    size: 180,
+  },
+  {
+    accessorKey: 'email',
+    header: 'E-mail',
+    size: 200,
+  },
+  {
+    accessorKey: 'username',
+    header: 'Username',
+    size: 150,
+    enableColumnFilter: false,
+  },
+  {
+    accessorKey: 'company',
+    header: 'Company Name',
+    size: 150,
+    enableColumnFilter: false,
+  },
+];
