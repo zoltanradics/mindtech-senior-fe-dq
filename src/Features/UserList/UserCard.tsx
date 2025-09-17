0a28c7438a54cf4c4c83701a5e9e3e6be697ea2f
Zoltan Radics <zoltan.radics@adnovum.hu>
2025-09-17 22:45:06 +0200

    Introduce UserCard component

---
+++ src/Features/UserList/UserCard.tsx
@@ -0,0 +1,61 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Container,
+  Typography,
+} from '@mui/material'
+import type { UserDto } from './User'
+
+interface UserCardProps {
+  user: UserDto
+  hideModal: () => void
+}
+
+export default function UserCard({ user, hideModal }: UserCardProps) {
+  return (
+    <Container>
+      <Card sx={{ borderBottom: 0 }}>
+        <CardHeader title={user?.name} subheader={user?.username} />
+        <CardContent>
+          <Typography
+            variant="body2"
+            sx={{ color: 'text.secondary', marginBottom: 2 }}
+          >
+            <strong>E-mail:</strong>
+            <br />
+            {user?.email}
+          </Typography>
+          <Typography
+            variant="body2"
+            sx={{ color: 'text.secondary', marginBottom: 2 }}
+          >
+            <strong>Phone:</strong>
+            <br />
+            {user?.phone}
+          </Typography>
+          <Typography
+            variant="body2"
+            sx={{ color: 'text.secondary', marginBottom: 2 }}
+          >
+            <strong>Company:</strong>
+            <br />
+            {user?.company.name}
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            <strong>City:</strong>
+            <br />
+            {user?.address.city}
+          </Typography>
+        </CardContent>
+      </Card>
+      <Box sx={{ marginTop: 2, marginBottom: 2 }}>
+        <Button onClick={hideModal} variant="outlined">
+          Close
+        </Button>
+      </Box>
+    </Container>
+  )
+}
